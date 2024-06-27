5518d416503cf7975d1d415300919e605634595c
Javier Areas <xareas@gmail.com>
2024-06-27 12:18:22 -0500

    Deploy website - based on dad693e5f8898c6bcbac4e6f86be53c3b807634f

---
+++ assets/js/3aaeb4c7.f5b50826.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknotification_center=self.webpackChunknotification_center||[]).push([[3273],{2741:t=>{t.exports=JSON.parse('{"metadata":{"permalink":"/arquitectura/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
